58db852d687626c196e0fab6137d65ff46cfd705
maalin2 <151789225+maalin2@users.noreply.github.com.>
2025-11-27 22:30:14 -0500

    slow pre-scheduled pattern with howler

--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -1,65 +1,105 @@
-import Image from "next/image";
+// to use hooks
+"use client";
+
+import { useRef, useEffect, useState } from 'react';
+import { Howl } from 'howler';
+//import { Pattern } from "@components/pattern";
+
+const bpm = 130;
+// 16th notes
+const stepsPerBeat = 4; 
+const stepDuration = (60 / bpm) * 1000 / stepsPerBeat;
+const steps = 32;
+
+const kick = new Howl({
+	src: ['./kick.wav'],
+	html5: true,
+	preload: true
+});
+
+const snare = new Howl({
+	src: ['./snare.wav'],
+	html5: true,
+	preload: true
+});
+
+const clap = new Howl({
+	src: ['./clap.wav'],
+	html5: true,
+	preload: true
+});
+
+const kickPattern = [
+	1, 0, 0, 0, 
+	1, 0, 0, 0, 
+	1, 1, 1, 1,
+	1, 1, 1, 1,
+	1, 0, 0, 0, 
+	1, 0, 0, 0, 
+	1, 1, 1, 1,
+	1, 1, 1, 1,
+];
+
+const snarePattern = [
+	0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0,
+	0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0
+];
+
+const clapPattern = [
+	0, 0, 0, 0, 0, 0, 1, 0,
+	0, 0, 0, 0, 0, 0, 1, 0,
+	0, 0, 0, 0, 0, 0, 1, 0,
+	0, 0, 0, 0, 0, 0, 1, 1
+];
 
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+	// when we mount or finish loading page, play simple pattern
+	// lookahead with requestAnimationBuffer
+	const [playing, setPlaying] = useState("");
+	const currentStep = useRef(0);
+	const nextStep = useRef(performance.now());
+
+	useEffect(() => {
+		const scheduleAheadTime = 100; // ms
+			
+		const scheduler = () => {
+			const now = performance.now();
+
+			while (nextStep.current < now + scheduleAheadTime && currentStep.current < steps) {
+				const step = currentStep.current;
+
+				let nowPlaying = '';
+
+				if (kickPattern[step] === 1) { nowPlaying += 'kick'; kick.play(); }
+				if (snarePattern[step] === 1) { nowPlaying += ' snare'; snare.play(); }
+				if (clapPattern[step] === 1) { nowPlaying += ' clap'; clap.play(); }
+
+				if (nowPlaying) setPlaying(nowPlaying);
+
+				currentStep.current += 1;
+				nextStep.current += stepDuration;
+			}
+
+
+			if (currentStep.current < steps) {
+				requestAnimationFrame(scheduler);
+			}
+		};
+
+		requestAnimationFrame(scheduler);
+	}, [])
+
+
+
+	return (
+		// flex-col to stack elements
+		// margin-5 for breathing room
+		<div className="flex flex-col m-5 items-center justify-center">
+		<h1 className="text-xl font-bold"> kick </h1>
+		<h1 className="text-xl font-bold"> snare </h1>
+		<h1 className="text-xl font-bold"> clap </h1>
+		<h1 className="text-xl font-bold"> now playing: {playing} </h1>
+		</div>
+	);
+	// add Pattern
 }
